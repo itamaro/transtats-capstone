14394785d95b111b83c4e6bd6042521510548155
Itamar Ostricher <itamarost@gmail.com>
2016-01-17 10:39:37 +0200

    measure size of filtered CSV instead of raw CSV

--- ETL/etl_extracted_zip.py
+++ ETL/etl_extracted_zip.py
@@ -1,10 +1,71 @@
 #!/usr/bin/python2.7
 
+from cStringIO import StringIO
+import csv
 import os
 import sys
 
 
 ACC_FILE = 'totsize'
+
+# commenting out fields that I don't want to keep
+FIELDS = [
+    # 'Year',
+    # 'Quarter',
+    # 'Month',
+    # 'DayofMonth',
+    'DayOfWeek',  # For G1-Q3
+    'FlightDate',
+    'UniqueCarrier',
+    # 'AirlineID',
+    # 'Carrier',
+    # 'TailNum',
+    'FlightNum',  # For Tom
+    'Origin',
+    # 'OriginCityName',
+    # 'OriginState',
+    # 'OriginStateFips',
+    # 'OriginStateName',
+    # 'OriginWac',
+    'Dest',
+    # 'DestCityName',
+    # 'DestState',
+    # 'DestStateFips',
+    # 'DestStateName',
+    # 'DestWac',
+    # 'CRSDepTime',
+    'DepTime',  # For Tom
+    'DepDelay',
+    # 'DepDelayMinutes',
+    # 'DepDel15',
+    # 'DepartureDelayGroups',
+    # 'DepTimeBlk',
+    # 'TaxiOut',
+    # 'WheelsOff',
+    # 'WheelsOn',
+    # 'TaxiIn',
+    # 'CRSArrTime',
+    'ArrTime',  # For Tom
+    'ArrDelay',
+    # 'ArrDelayMinutes',
+    # 'ArrDel15',
+    # 'ArrivalDelayGroups',
+    # 'ArrTimeBlk',
+    # 'Cancelled',
+    # 'CancellationCode',
+    # 'Diverted',
+    # 'CRSElapsedTime',
+    # 'ActualElapsedTime',
+    # 'AirTime',
+    'Flights',
+    # 'Distance',
+    # 'DistanceGroup',
+    # 'CarrierDelay',
+    # 'WeatherDelay',
+    # 'NASDelay',
+    # 'SecurityDelay',
+    # 'LateAircraftDelay',
+]
 
 
 def accumulate_size(size):
@@ -21,8 +82,16 @@ def etl_dir(base_dir, *args):
     for filename in files:
       if filename.lower().endswith('.csv'):
         fullpath = os.path.join(root, filename)
-        csvsize = os.path.getsize(fullpath)
-        print filename, 'size is', csvsize
+        with open(fullpath, 'rb') as csvfile:
+          out_csv_buff = StringIO()
+          writer = csv.DictWriter(out_csv_buff, fieldnames=FIELDS)
+          writer.writeheader()
+          for row in csv.DictReader(csvfile):
+            writer.writerow({field: row[field] for field in FIELDS})
+        csvsize = len(out_csv_buff.getvalue())
+        out_csv_buff.close()
+        del out_csv_buff
+        print 'filtered', filename, 'size is', csvsize
         accumulate_size(csvsize)
 
 
