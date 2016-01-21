14127b9beb6fbbfae72aae7694e11b3d814fad74
Itamar Ostricher <itamarost@gmail.com>
2016-01-21 09:02:43 +0200

    upload transformed CSV's to S3 in ETL

--- ETL/etl.sh
+++ ETL/etl.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+usage_and_die() {
+  echo "$0 [s3_dest_path]" >&2
+  echo "DATA_BASE_DIR must hold a valid readable directory path" >&2
+  exit 42
+}
+
 DATA_BASE_DIR="/mnt/data/aviation/airline_ontime"
+S3_DIR="$1"
+
+if [ ! -d "$DATA_BASE_DIR" ]; then
+  usage_and_die
+fi
+if [[ ! "$S3_DIR" == s3* ]]; then
+  usage_and_die
+fi
+
 echo "0" > "totsize"
-find $DATA_BASE_DIR -iname "*.zip" -type f -exec bash etl_one_zip.sh {} \;
+find $DATA_BASE_DIR -iname "*.zip" -type f -exec bash etl_one_zip.sh {} $S3_DIR \;
--- ETL/etl_one_zip.sh
+++ ETL/etl_one_zip.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+
+usage_and_die() {
+  echo "$0 [zipfile] [s3_dest_path]" >&2
+  exit 42
+}
+
 ZIP_FILE="$1"
+S3_DIR="$2"
+
+if [ ! -r "$ZIP_FILE" ]; then
+  usage_and_die
+fi
+if [[ ! "$S3_DIR" == s3* ]]; then
+  usage_and_die
+fi
+
 EXDIR="$(mktemp -d)"
 TRANSDIR="$(mktemp -d)"
 echo "Extracting $ZIP_FILE to $EXDIR, writing transformed CSV's to $TRANSDIR"
 unzip -q "$ZIP_FILE" -d "$EXDIR"
 python etl_extracted_zip.py "$EXDIR" "$TRANSDIR"
 rm -r "$EXDIR"
-# TODO: load files in $TRANSDIR to HDFS
+echo "Uploading $( ls $TRANSDIR | wc -l ) CSV's to S3 bucket at $S3_DIR: $( ls $TRANSDIR )"
+aws s3 cp "$TRANSDIR" "$S3_DIR" --recursive
 rm -r "$TRANSDIR"
