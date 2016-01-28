139420cdbd3d4a33f3df91d778a812ed3d34d0c1
Itamar Ostricher <itamarost@gmail.com>
2016-01-28 13:30:55 +0200

    add mapper for formatting key/value's before loading to Hive

---
+++ MapReduce/format_for_hive_mapper.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import sys
+
+if '__main__' == __name__:
+  for line in sys.stdin:
+    parts = line.strip().split()
+    key_parts = parts[0].strip().split(':')
+    print '\x01'.join(parts[:1] + key_parts + parts[1:])
